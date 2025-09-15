724d0ecc0165c839a723f050d41fb243a0dc6c7c
Midanka Lahon <midankalahon@gmail.com>
2025-09-15 20:01:52 +0530

    added per skill screen and change some ui elements

--- app/src/main/java/com/example/pathfinder/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/pathfinder/ui/screens/HomeScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
--- app/src/main/java/com/example/pathfinder/ui/screens/PathfinderApp.kt
+++ app/src/main/java/com/example/pathfinder/ui/screens/PathfinderApp.kt
@@ -1,6 +1,5 @@
 package com.example.pathfinder.ui.screens
 
-import android.R.attr.type
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
